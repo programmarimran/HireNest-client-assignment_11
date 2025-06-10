efaccaedea11231071754739671ff5e7a3696d10
programmarimran <programmarimran@gmail.com>
2025-06-11 00:56:10 +0600

    added a popular services is totally created

--- src/Pages/services/Home.jsx
+++ src/Pages/services/Home.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { use } from "react";
 import { useLoaderData } from "react-router";
 import ServiceCard from "../../components/ServiceCard";
 import Hero from "../../components/Hero";
-
+import ServiceContext from "../../contexts/ServiceContext";
 
 const Home = () => {
+  const {darkIstrue}=use(ServiceContext)
   const services = useLoaderData();
   console.log(services);
   return (
     <>
-    <div className=" pt-6">
-      <Hero services={services}></Hero>
-    </div>
-    <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 py-12">
-      {services?.map((service) => (
-        <ServiceCard key={service._id} service={service}></ServiceCard>
-      ))}
-    </div>
+      <div className=" pt-6">
+        <Hero services={services}></Hero>
+      </div>
+      <section className="py-16 pb-4">
+        <h2
+          className={`text-2xl md:text-3xl font-bold text-center mb-2 ${
+            darkIstrue ? "text-white" : "text-black"
+          }`}
+        >
+          🌟 Popular Services
+        </h2>
+        <p
+          className={`text-center max-w-xl mx-auto mb-6 ${
+            darkIstrue ? "text-gray-300" : "text-gray-600"
+          }`}
+        >
+          Discover the most booked and top-rated services from our trusted
+          providers. Get quality service at your doorstep – fast, easy, and
+          reliable!
+        </p>
+
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 ">
+          {services?.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,17 +18,29 @@ const Hero = ({ services }) => {
       >
         {services?.map((service) => (
           <SwiperSlide key={service._id}>
-            <div className="hero h-[180px] md:h-[400px] rounded-2xl overflow-hidden relative">
-              <img className=" object-cover object-bottom" src={service?.imageUrl} alt="image" />
+            <div className={`hero h-[180px] md:h-[400px] rounded-2xl overflow-hidden relative ${darkIstrue?"border border-gray-500":""}`}>
+              <img
+                className=" object-cover object-bottom rounded-2xl"
+                src={service?.imageUrl}
+                alt="image"
+              />
               <div className="absolute inset-0 bg-gray-800/60  flex items-center justify-center  text-center px-4">
                 <div className="max-w-md text-white z-50">
                   <h2 className="text-xl md:text-3xl font-bold mb-2">
                     {service.serviceName}
                   </h2>
-                  <p className="text-sm md:text-base">
-                    🧑‍🔧 <strong>{service.provider.name}</strong> | 📍{" "}
-                    {service.serviceArea}
-                  </p>
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={service.provider.photoUrl}
+                      alt={service.provider.name}
+                      className="w-8 h-8 rounded-full"
+                    />
+                    <strong className={`text-sm md:text-base`}>
+                      {service.provider.name}
+                    </strong>
+                    <span>| 📍 {service.serviceArea}</span>
+                  </div>
+
                   <p className="text-sm md:text-base mt-1">
                     💰 <span className="font-semibold">৳{service.price}</span>
                   </p>
